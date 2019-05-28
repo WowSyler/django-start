341518154dce5973f1998ab7b16f55f95c632237
ozan Küçük <ozankck@msn.com>
2019-05-28 22:43:48 +0300

    upp

--- HomeWork/settings.py
+++ HomeWork/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'HomeWork.urls'
@@ -129,9 +130,7 @@ STATICFILES_DIRS = (
 
 
 
-
-
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
