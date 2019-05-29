e70c742228838899f521ed22dbb2646c0218be33
ozan Küçük <ozankck@msn.com>
2019-05-29 19:46:11 +0300

    upsdarte

--- HomeWork/settings.py
+++ HomeWork/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = '78c-44p2td=v&&4+8&9sek@_2_atfqebpt(a#-3d-hxpum^vh4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -129,6 +129,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
-ALLOWED_HOSTS = ['*']
-django_heroku.settings(locals())
