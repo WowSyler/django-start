9200e1ce94eff6ac61e75cc839091e6077641416
ozan Küçük <ozankck@msn.com>
2019-05-29 20:02:00 +0300

    s

--- HomeWork/wsgi.py
+++ HomeWork/wsgi.py
@@ -10,12 +10,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from homework.wsgi import HomeWork
 
-from whitenoise.django import DjangoWhiteNoise
 
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HomeWork.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = HomeWork(application)
+
